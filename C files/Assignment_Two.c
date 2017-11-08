626d0b70473b92b7e41bd19f82dac235d636ca93
TerenceLau <Terenceliu1012@gmail.com>
2017-11-08 22:37:37 +0800

    update

--- C files/Assignment_Two.c
+++ C files/Assignment_Two.c
@@ -19,15 +19,15 @@ int main(void)
 	printf("lack of a zero, it should be 'return 0'");
 	
 	//This is the answer of Question Four
-	printf("---1");
-	printf("---2");
-	printf("---3");
-	printf("---4");
-	printf("---5");
-	printf("---6");
-	printf("---7");
-	printf("---8");
-	printf("---9");
+	printf("----1");
+	printf("----2");
+	printf("----3");
+	printf("----4");
+	printf("----5");
+	printf("----6");
+	printf("----7");
+	printf("----8");
+	printf("----9");
 	printf("---10");
 	printf("\n");
 	
